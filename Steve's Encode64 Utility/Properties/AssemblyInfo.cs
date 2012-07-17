e328e0fe62dec5a084c93943175b84b5f2098a56
Steven Benner <admin@stevenbenner.com>
2012-07-17 03:43:56 -0700

    Updated assembly info.

--- Steve's Encode64 Utility/Properties/AssemblyInfo.cs
+++ Steve's Encode64 Utility/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Steve's Encode64 Utility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Encode64 Utility Executable")]
+[assembly: AssemblyDescription("Utility to encode files in base 64.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Steve's Encode64 Utility")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
