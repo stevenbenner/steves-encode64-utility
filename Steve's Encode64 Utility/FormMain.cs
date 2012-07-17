dcb3097415a226385d6e7ac3c7ee45262555f8fc
Steven Benner <admin@stevenbenner.com>
2012-07-17 03:51:10 -0700

    Updated file info comments.

--- Steve's Encode64 Utility/FormMain.cs
+++ Steve's Encode64 Utility/FormMain.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.IO;
-using System.Windows.Forms;
-
-/** Steve's Encode64 Utility
- ** Copyright © 2012 Steven Benner.
- ** http://stevenbenner.com/
+﻿/*!
+ * Steve's Encode64 Utility
+ * Copyright © 2012 Steven Benner.
+ * https://github.com/stevenbenner/steves-encode64-utility
  * 
  * A simple tool to base64 encode files and grab the
  * encoded string. Useful for data URI creation.
  */
+
+using System;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Steves_Encode64_Utility
 {
